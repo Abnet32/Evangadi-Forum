162afe664f230e2a0bf4d4bbc9b04c8304600316
Abnet Mekonen <abnetmekonen32@gmail.com>
2025-10-30 10:46:46 -0400

    Update app.js

--- server/app.js
+++ server/app.js
@@ -31,6 +31,7 @@ const limiter = rateLimit({
   message: "Too many requests from this IP, please try again later.",
 });
 app.use(limiter);
+
 // -------------------------------
 // Test route
 // -------------------------------
